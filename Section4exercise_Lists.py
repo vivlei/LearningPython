fc471ace57bd8f4dba22c4f1817ca267ababb129
vivlei <50153044+vivlei@users.noreply.github.com>
2019-04-30 13:06:21 -0700

    Add files via upload

---
+++ Section4exercise_Lists.py
@@ -0,0 +1,15 @@
+job_applicants = ["John","Geoff","Brady","Thomas"]
+heights = [61, 70, 67, 64, 65]
+ints_and_Strings = [1,2,3,'four','five']
+empty_list = []
+
+print(job_applicants)
+def text(print_this):
+    print(print_this)
+text("David is Awesome!!")
+
+print()
+#for item in job_Applicants:
+# print(itme)
+
+print("a name is: " + job_applicants[2])
