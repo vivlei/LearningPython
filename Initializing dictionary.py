0a476e1f5a1550c3a792ab25e55fe282047e0e69
vivlei <50153044+vivlei@users.noreply.github.com>
2019-05-07 12:46:19 -0700

    Add files via upload

---
+++ Initializing dictionary.py
@@ -0,0 +1,7 @@
+my_dict = {} #initializing the dict
+my_dict["new_key"] = "new_value"
+print(my_dict)
+menu = {"oatmeal":3,"AVOCADO TOAST":6,"CARROT JUICE":5,"BLUEBERRY MUFFIN":2}
+print(menu)
+menu["cheesecake"] = 8
+print(menu)
